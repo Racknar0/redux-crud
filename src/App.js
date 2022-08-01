c94f6c6d634ace7620710cdc2a00266eb9b9581e
Camilo Torres <racknarow1@gmail.com>
2022-08-01 13:29:28 -0500

    add actions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from "./components/Header";
+import Header from './components/Header';
 import NuevoProducto from './components/NuevoProducto';
-import Productos from "./components/Productos";
-import EditarProducto from "./components/EditarProducto";
+import Productos from './components/Productos';
+import EditarProducto from './components/EditarProducto';
 
- 
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
-  return (
-    <Router>
-      <Header />
-        <div className="container">
-          <Routes >
-            <Route path="/" element={<Productos/>}/>
-            <Route path="/productos/nuevo" element={<NuevoProducto/>}/>
-            <Route path="/productos/editar/:id" element={<EditarProducto/>}/>
-          </Routes >
-        </div>
-    </Router>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <Header />
+                <div className="container">
+                    <Routes>
+                        <Route path="/" element={<Productos />} />
+                        <Route
+                            path="/productos/nuevo"
+                            element={<NuevoProducto />}
+                        />
+                        <Route
+                            path="/productos/editar/:id"
+                            element={<EditarProducto />}
+                        />
+                    </Routes>
+                </div>
+            </Router>
+        </Provider>
+    );
 }
- 
+
 export default App;
---
+++ src/reducers/productosReducer.js
@@ -0,0 +1,19 @@
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR,
+} from '../types';
+
+//cada reducer tiene su propio state
+const initialState = {
+    productos: [],
+    error: null,
+    loading: false,
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
