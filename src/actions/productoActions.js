c9b207d01dffff3694631ad0b9bf42140dd02af0
Camilo Torres <racknarow1@gmail.com>
2022-08-01 16:15:34 -0500

    add axios

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -1,3 +1,4 @@
+import clienteAxios from '../config/axios';
 import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
@@ -6,7 +7,36 @@ import {
 
 //Crear nuevos productos
 export function crearNuevoProductoAction(producto) {
-    return () => {
-        console.log(producto);
+    return async (dispatch) => {
+        dispatch(agregarProducto());
+
+        try {
+            // insertar en la api
+            await clienteAxios.post('/productos', producto);
+
+            // Si todo sale bien actualizar error
+            dispatch(agregarProductoExito(producto));
+        } catch (error) {
+            console.log(error);
+            //Si hay un error cambiar el state
+            dispatch(agregarProductoError(true));
+        }
     };
 }
+
+const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO,
+    payload: true,
+});
+
+//Si el producto se guarda en la BD
+const agregarProductoExito = (producto) => ({
+    type: AGREGAR_PRODUCTO_EXITO,
+    payload: producto,
+});
+
+// Si hubo un error
+const agregarProductoError = (estado) => ({
+    type: AGREGAR_PRODUCTO_ERROR,
+    payload: estado,
+});
--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -11,8 +11,29 @@ const initialState = {
     loading: false,
 };
 
+// eslint-disable-next-line
 export default function (state = initialState, action) {
     switch (action.type) {
+        case AGREGAR_PRODUCTO:
+            return {
+                ...state,
+                loading: action.payload,
+            };
+
+        case AGREGAR_PRODUCTO_EXITO:
+            return {
+                ...state,
+                loading: false,
+                productos: [...state.productos, action.payload],
+            };
+
+        case AGREGAR_PRODUCTO_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+            };
+
         default:
             return state;
     }
